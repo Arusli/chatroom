f5af7bd6100ddfceb4e3bc4973545c182a5b05b7
Arusli <andreworusli@gmail.com>
2023-12-24 20:30:46 -0500

    add colored names

--- src/constants/users.ts
+++ src/constants/users.ts
@@ -2,41 +2,48 @@ export type User = {
   name: string;
   id: number;
   message: string;
+  color: string;
 };
 
 export const users: User[] = [
   {
-    name: "Andrew",
+    name: `Andrew`,
     id: 1,
     message: `its me andrew, whats up?`,
+    color: `blue`,
   },
   {
-    name: "David",
+    name: `David`,
     id: 2,
-    message: 'hey andrew. David here.',
-  }
+    message: `hey andrew. David here.`,
+    color: `red`,
+  },
 ];
 
 export const users2: User[] = [
-    {
-      name: "Andrew",
-      id: 1,
-      message: `its me andrew, whats up?`,
-    },
-    {
-      name: "David",
-      id: 2,
-      message: 'hey andrew. David here.',
-    },
-    {
-        name: "Robert",
-        id: 3,
-        message: "I'll join"
-    }
-  ];
+  {
+    name: `Andrew`,
+    id: 1,
+    message: `its me andrew, whats up?`,
+    color: `blue`,
+  },
+  {
+    name: `David`,
+    id: 2,
+    message: `hey andrew. David here.`,
+    color: `red`,
+  },
+  {
+    name: `Robert`,
+    id: 3,
+    message: `I'll join`,
+    color: `green`,
+  },
+];
 
 export const newUser: User = {
-    name: "Robert",
-    id: 3,
-    message: "I'll join"
-}
+  name: `Robert`,
+  id: 3,
+  message: `I'll join`,
+  color: `green`,
+};
